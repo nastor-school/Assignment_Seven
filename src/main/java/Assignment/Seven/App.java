35a6f3764d7d26977f650adf9ba2fd0571c4e751
Nick Astor <nastor@mail.valenciacolleg.edu>
2022-07-05 19:25:20 -0700

    Docker working

--- src/main/java/Assignment/Seven/App.java
+++ src/main/java/Assignment/Seven/App.java
@@ -2,6 +2,10 @@ package Assignment.Seven;
 
 public class App 
 {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+	
     public static double add(int num1, int num2) {
         return num1 + num2;
     }
